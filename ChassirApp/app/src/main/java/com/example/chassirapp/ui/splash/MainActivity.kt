957fea0fc2b50f1afb886abf259a3225599d4a14
Ilham Hendro Prasetyo <ilhamhendroprasetyo.com>
2023-08-08 17:35:16 +0700

    Chassir App

---
+++ ChassirApp/app/src/main/java/com/example/chassirapp/ui/splash/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.chassirapp.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.chassirapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
